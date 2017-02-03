22b980f365b3fd3d21347f6663babbf78db824d6
Max Jacobson <max@hardscrabble.net>
2017-02-02 23:06:32 -0500

    implement regular for person

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+struct Person {
+    name: String,
+}
+
+trait Regular {
+    fn save(&self);
+    fn to_s(&self) -> String;
+}
+
+impl Regular for Person {
+    fn save(&self) {
+        let path = "./persons.txt";
+        let mut f = File::create(path).unwrap();
+        f.write(self.to_s().as_bytes()).unwrap();
+    }
+
+    fn to_s(&self) -> String {
+        format!("{}", self.name)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let max = Person { name: String::from("Maxwell") };
+
+    max.save();
 }
